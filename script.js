e9c05059a72c22b974b097f634786c5275f53d55
AliHaidry <alihaidry11@gmail.com>
2021-02-13 21:36:35 -0500

    code update

---
+++ script.js
@@ -0,0 +1,20 @@
+/*************************
+/*What is the DOM*/
+/*************************/
+
+// Document Object Model.
+// Tree of nodes / elements created by browser.
+// Javascript can be used to read / write / manipulate to the DOM.
+// Object Oriented Representation.
+
+/** These are the building blocks of the language and they are critical but now we're going to get to actually use this stuff in 
+ * combination with the browser with with the DOM. All right. So what is the DOM? It stands for the document object model 
+ * and it's basically a structured representation of an HTML document. It can be thought of kind of as a tree of nodes or elements 
+ * created by the browser. Okay, and when I say node or element, I mean in any of the HTML tags the HTML tag, the body tag, 
+ * each one's pretty much anything that is in the document and we can use JavaScript to manipulate these DOM elements or nodes. 
+ * The DOM is object oriented, meaning that each node has its own set of properties and methods that we can change, we can add, 
+ * we can remove. Now the browser gives us a window object and inside that we have a document object.*/
+
+/*************************
+/*What is the DOM END*/
+/*************************/
