d4b6df34297508c21ae813d9c54f899e37cf9e87
AliHaidry <alihaidry11@gmail.com>
2021-02-17 15:36:09 -0500

    code update

--- script.js
+++ script.js
@@ -401,3 +401,47 @@
 /*************************
 /* Event listeners and the event object END*/
 /*************************/
+
+
+/*************************
+/* Mouse Events */
+/*************************/
+
+// const clearBtn = document.querySelector('.clear-tasks');
+// const card = document.querySelector('.card');
+// const heading = document.querySelector('h5');
+
+// // Click
+// // clearBtn.addEventListener('click', runEvent);
+// // Doubleclick
+// //clearBtn.addEventListener('dbclick', runEvent);
+
+// // Mousedown
+// //clearBtn.addEventListener('mousedown', runEvent);
+// // MouseUp
+// //card.addEventListener('mouseup', runEvent);
+// // Mouseenter
+// //card.addEventListener('mouseenter', runEvent);
+// // Mouseleave
+// // card.addEventListener('mouseleave', runEvent);
+// // // Mouseover
+// // card.addEventListener('mouseover', runEvent);
+// // // Mouseout
+// // card.addEventListener('mouseout', runEvent);
+
+// // Mousemove
+// card.addEventListener('mousemove', runEvent);
+
+// // Event handlers
+// function runEvent(e){
+//     console.log(`EVENT TYPE: ${e.type}`);
+
+
+//     heading.textContent = `MouseX: ${e.offsetX} MouseY: ${e.offsetY}`;
+
+//     document.body.style.background = `rgb(${e.offsetX}, ${e.offsetY}, 40)`;
+// }
+
+/*************************
+/* Mouse Events END*/
+/*************************/
