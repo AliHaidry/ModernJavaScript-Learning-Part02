7830f25acbb010ac7d7d723cdb6f2eb5e95e268f
AliHaidry <alihaidry11@gmail.com>
2021-02-17 14:58:02 -0500

    code update

--- script.js
+++ script.js
@@ -349,3 +349,55 @@
 /*************************
 /* Removing and replacing elements END*/
 /*************************/
+
+
+/*************************
+/* Event listeners and the event object*/
+/*************************/
+// document.querySelector('.clear-tasks').addEventListener('click', 
+//     function(e){                                 // this is called as a callback function.
+//         console.log('Hello World');
+
+//    //     e.preventDefault();
+// });
+
+
+// document.querySelector('.clear-tasks').addEventListener('click',
+// onClick);
+
+// function onClick(e){
+//    // console.log('Clicked');
+
+//    let val;
+
+//    val = e;
+
+//    // Event target element
+//    val = e.target;
+//    val = e.target.id;
+//    val = e.target.className;
+//    val = e.target.classList;
+
+//    // Event type
+//    val = e.type;
+   
+//    // timestamp
+//    val = e.timestamp;
+
+//    // coordinates event relative to the window
+//    val = e.clientY;
+//    val = e.clientX;
+
+//    // coordinates event relative to the element
+//    val = e.offsetY;
+//    val = e.offsetX
+
+
+
+//    console.log(val);
+
+//}
+
+/*************************
+/* Event listeners and the event object END*/
+/*************************/
