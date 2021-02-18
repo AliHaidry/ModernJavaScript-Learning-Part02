020750cb2391a3c335892ed3fd0570d2fd4e9d11
AliHaidry <alihaidry11@gmail.com>
2021-02-18 09:43:13 -0500

    code update

--- script.js
+++ script.js
@@ -445,3 +445,59 @@
 /*************************
 /* Mouse Events END*/
 /*************************/
+
+/*************************
+/* Keyboard & Input Events*/
+/*************************/
+
+const form = document.querySelector('form');
+const taskInput = document.getElementById('task');
+const heading = document.querySelector('h5');
+
+// clear input
+taskInput.value = "";
+
+//form.addEventListener('submit', runEvent);
+
+//Keydown
+//taskInput.addEventListener('keydown',runEvent);
+
+//Keydown
+//taskInput.addEventListener('keyup',runEvent);
+
+// Keypress
+//taskInput.addEventListener('keypress', runEvent);
+
+// Focus
+//taskInput.addEventListener('focus', runEvent);
+
+// Blur
+//taskInput.addEventListener('blur',runEvent);
+
+// Cut
+//taskInput.addEventListener('cut',runEvent);
+
+// Paste
+//taskInput.addEventListener('paste', runEvent);
+
+// Input 
+taskInput.addEventListener('input',runEvent);
+
+
+
+function runEvent(e)
+{
+    console.log(`EVENT TYPE: ${e.type}`);
+
+    //console.log(e.target.value);
+
+//  heading.innerHTML = e.target.value;
+//     // Get input value.
+//     console.log(taskInput.value);
+
+//     e.preventDefault(); // use it when working with forms.
+}
+
+/*************************
+/* Keyboard & Input Events END*/
+/*************************/
