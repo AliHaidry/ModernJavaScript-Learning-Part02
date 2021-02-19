6e7067a8039ce1f356d59e36decf9ee98159882d
AliHaidry <alihaidry11@gmail.com>
2021-02-19 14:09:04 -0500

    code update for DOM project

--- script.js
+++ script.js
@@ -15,6 +15,12 @@ loadEventListeners();
 function loadEventListeners(){
     // Add task event
     form.addEventListener('submit',addTask);
+    // Remove task event
+    taskList.addEventListener('click',removeTask);
+    // Clear task event 
+    clearBtn.addEventListener('click',clearTasks);
+    // Filter tasks event 
+    filter.addEventListener('keyup', filterTasks);
 }
 
 // Add task function
@@ -52,4 +58,43 @@ function addTask(e){
     taskInput.value='';
 
     e.preventDefault();
+}
+
+// Remove Task 
+function removeTask(e) {
+    if(e.target.parentElement.classList.contains('delete-item'))
+    {
+        if(confirm('Are you sure you want to delete this task?'))
+        {
+           e.target.parentElement.parentNode.remove();
+        }
+    }
+}
+
+// Clear Task
+function clearTasks(){
+   // taskList.innerHTML = ''; // one-way to do it.
+   
+   // Faster way
+   while(taskList.firstChild)
+   {
+    taskList.removeChild(taskList.firstChild);
+   }
+
+}
+
+// Filter tasks
+function filterTasks(e){
+    const text = e.target.value.toLowerCase();
+    document.querySelectorAll('.collection-item').forEach(function(task){
+        const item = task.firstChild.textContent;
+        if(item.toLowerCase().indexOf(text) != -1)
+        {
+            task.style.display ='block';
+        }
+        else
+        {
+            task.style.display = 'none';
+        }
+    });
 }
